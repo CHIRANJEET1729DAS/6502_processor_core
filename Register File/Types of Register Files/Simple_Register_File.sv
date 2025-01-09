1969b9de2641f1314ed69ed6fa8a989ff26915f7
CHIRANJEET1729DAS <126919390+CHIRANJEET1729DAS@users.noreply.github.com>
2025-01-09 13:43:58 +0530

    Update Simple_Register_File.sv

--- Register File/Types of Register Files/Simple_Register_File.sv
+++ Register File/Types of Register Files/Simple_Register_File.sv
@@ -1,6 +1,6 @@
 module single_port_register_file (input wire clk,input wire reset,input wire write_enable,input wire [2:0] write_address,input wire [3:0] read_address,input wire [15:0] data_in,output reg [15:0] data_out);
  reg [15:0] reg_file [15:0];
- always @(posedge clk or posedge reset or posedge write_enable)begin
+ always @(posedge clk or posedge reset)begin
 	 if(reset)begin
 	      for (i=0;i<=16;i=i+1)begin
 		 reg_file[i] <= 16'b0;
